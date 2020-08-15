54615bca5ad49d997013a83ce6bb0cc55fc6b293
abuzubai <69711002+abuzubai@users.noreply.github.com>
2020-08-15 13:35:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Initialize Firebase
 
 
-///Edit Under This only
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.18.0/firebase-app.js"></script>
 
-https://github.com/SpeedForceX
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.18.0/firebase-analytics.js"></script>
 
-     
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
