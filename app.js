b144eb15f59d401a0de2adcc09f9bf2d95ec43d0
abuzubai <69711002+abuzubai@users.noreply.github.com>
2020-08-15 13:22:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,7 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
+https://github.com/SpeedForceX
 
      
  
